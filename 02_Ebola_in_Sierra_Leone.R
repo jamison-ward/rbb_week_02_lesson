c20fd441860bf60680cbd29bb06da015bd1eb51a
Jamison-Ward <oceanic.magma682@simplelogin.fr>
2025-10-07 22:05:02 -0400

    After data input

--- 02_Ebola_in_Sierra_Leone.R
+++ 02_Ebola_in_Sierra_Leone.R
@@ -1,3 +1,25 @@
-# Author: Jamison M. Ward
+# Ebola Sierra Leone Analysis
+# Author: Jamison Ward
 # Created: Tue, Oct 07 2025, 21:33:52
 
+
+# Load Packages -----------------------------------------------------------
+
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(
+  tidyverse,  # meta-package
+  plotly,
+  janitor,
+  visdat,
+  esquisse
+)
+
+# To easily import data in tabular form, File -> Import Dataset -> From Text (readr)
+# This function uses the readr library to important and optionally format the tabular data
+# IMPORTANT: After importing the data, create a new section to load the tabular data into RStudio
+  # This is especially important when sharing the code with others
+
+
+# Load Data ---------------------------------------------------------------
+
+ebola_sierra_leone <- read_csv("ebola_sierra_leone.csv")
